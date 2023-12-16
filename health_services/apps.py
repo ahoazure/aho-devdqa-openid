323ff43615264298b1f50b47d309c7448da67459
Stephen Mburu <ahoazure@gmail.com>
2023-12-16 19:46:17 +0300

    Changed foreign Kenya reference from UHCClockIndicators to StgIndicators from One-to-Many to One-to-One relationship

--- health_services/apps.py
+++ health_services/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class HealthServicesConfig(AppConfig):
     name = 'health_services'
-    verbose_name = _('Health-Services')
+    verbose_name = _('Health Services')
